cc11a7e8a7986e6a12c47d367f3a873e92c42e3a
Mojin2 <aiduriaaa@gmail.com>
2023-02-16 21:56:31 +0900

    updated mini_blog code

--- REACT/mini-blog/src/App.js
+++ REACT/mini-blog/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import styled from "styled-components";
+import MainPage from "./components/page/MainPage";
+import PostWritePage from "./components/page/PostWritePage";
+import PostViewPage from "./components/page/PostViewPage";
+
+const MainTitleText=styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+function App(props){
+  return(
+    <BrowserRouter>
+      <MainTitleText>미니 블로그 프로젝트</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage/>}/>
+        <Route path="post-write" element={<PostWritePage/>}/>
+        <Route path="post/:postId" element={<PostViewPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
